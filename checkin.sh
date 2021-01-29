0a761b9e0deafdde7478bda623f2c3fd8a76612c
James Gray <james.gray@mvfglobal.com>
2021-01-29 16:03:05 +0000

    install docker compose

--- checkin.sh
+++ checkin.sh
@@ -135,6 +135,15 @@ install_docker() {
   sudo gpasswd -a "$USER" docker
 }
 
+install_docker_compose() {
+  [[ -f "/usr/local/bin/docker-compose" ]] && return 0
+
+  sudo curl \
+        -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" \
+        -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+}
+
 install_antigen() {
   if [ ! -f ~/Projects/antigen.zsh ]; then
     echo "Installing antigen..."
@@ -253,6 +262,7 @@ install_spotify
 install_slack
 install_discord
 install_docker
+install_docker_compose
 install_antigen
 install_rbenv
 install_nvm
